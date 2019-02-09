77811d1154ada070d5430a9aa62ab4ed3647b658
clark7511 <47412607+clark7511@users.noreply.github.com>
2019-02-09 22:08:35 +0800

    Create list_learning.py
    list leraning for sample code

---
+++ list_learning.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+empty_list=[] #空串列 
+
+
+# In[2]:
+
+
+weekdays=['Monday','Tuesday','Wednesday','Friday',]
+#list of str, 元素是string
+#注意,最後的逗號可有可無
+
+
+# In[3]:
+
+
+weekdays
+
+
+# In[4]:
+
+
+another_empty_list=list()
+
+
+# In[5]:
+
+
+another_empty_list
+
+
+# In[6]:
+
+
+list_c=list('apple')
+
+
+# In[7]:
+
+
+list_c
+
+
+# In[8]:
+
+
+tuple_s=('ready','fire','aim')
+
+
+# In[9]:
+
+
+tuple_s
+
+
+# In[10]:
+
+
+list_t=list(tuple_s)
+
+
+# In[11]:
+
+
+list_t
+
+
+# In[12]:
+
+
+birthday='1986/1/1' 
+
+
+# In[13]:
+
+
+bd_list=birthday.split('/')
+
+
+# In[14]:
+
+
+bd_list
+
+
+# In[16]:
+
+
+student_01='s044,clark yeh,cs,09XXXXXXXX'
+
+
+# In[17]:
+
+
+sstu_01_list=student_01.split(',')
+
+
+# In[18]:
+
+
+sstu_01_list
+
+
+# In[19]:
+
+
+city=['Kaohsiung','Tainai','Taipei','Kaohsiung']
+
+
+# In[20]:
+
+
+city[0] 
+
+
+# In[21]:
+
+
+city[2] 
+
+
+# In[23]:
+
+
+city[5]
+
+
+# In[25]:
+
+
+city[0:2]
+
+
+# In[24]:
+
+
+city[0:2]
+
+
+# In[26]:
+
+
+city[::2]
+
+
+# In[27]:
+
+
+city[1::2]
+
+
+# In[28]:
+
+
+list_1=[34,67,66,'ok','cont.']
+
+
+# In[29]:
+
+
+list_2=[55,77,'ok','cont.']
+
+
+# In[30]:
+
+
+list_3=['aaa','bbb']
+
+
+# In[31]:
+
+
+list_all=[list_1,list_2,'sss',list_3] 
+
+
+# In[32]:
+
+
+list_all
+
+
+# In[33]:
+
+
+list_all[0]
+
+
+# In[34]:
+
+
+list_all[-2]
+
+
+# In[35]:
+
+
+list_all[1][1]
+
+
+# In[ ]:
+
+
+
+
